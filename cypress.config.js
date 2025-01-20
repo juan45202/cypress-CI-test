e92386266d32fde36c8c3416d74d6a58d39f2488
juan45202 <jjrm.son8@gmail.com>
2025-01-20 18:40:34 -0500

    pipe 2

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from "cypress";
-// import cymapTasks from "cymap/src/cymapTasks.js";
+import cymapTasks from "cymap/src/cymapTasks.js";
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       on("task",{
-        // ...cymapTasks
+        ...cymapTasks
       })
       return config;
       // implement node event listeners here
