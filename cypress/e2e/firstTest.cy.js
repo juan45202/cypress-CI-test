71148fbb73b95631d16de9fb73270eebeab6f16e
juan45202 <jjrm.son8@gmail.com>
2025-01-20 18:50:20 -0500

    pipe 2

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -1,21 +1,12 @@
 
 describe('My First Test', () => {
-    
-    /*{
-        password: Cypress.env('vdso vtxq miht ydwq'),
-        user: Cypress.env('user'),
-        host: Cypress.env('host'),
-        port: Cypress.env('port'),
-        tls:true,
-        tlsOptions: { rejectUnauthorized: false }
-    }*/
 
     it('Visits the Kitchen Sink', () => {
         cy.setConnectionConfig( {
-            password: "vdso vtxq miht ydwq",
-            user: "juanchomon45@gmail.com",
-            host: "imap.gmail.com",
-            port: 993,
+            password: Cypress.env('vdso vtxq miht ydwq'),
+            user: Cypress.env('user'),
+            host: Cypress.env('host'),
+            port: Cypress.env('port'),
             tls:true,
             tlsOptions: { rejectUnauthorized: false }
         });
