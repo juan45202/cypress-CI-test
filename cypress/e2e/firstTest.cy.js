7d7060ffd1ae758df6a28c7a5e41728aa0eed71e
juan45202 <jjrm.son8@gmail.com>
2025-01-20 17:37:55 -0500

    Pipeline integration 5

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -1,12 +1,21 @@
 
 describe('My First Test', () => {
     
+    /*{
+        password: Cypress.env('vdso vtxq miht ydwq'),
+        user: Cypress.env('user'),
+        host: Cypress.env('host'),
+        port: Cypress.env('port'),
+        tls:true,
+        tlsOptions: { rejectUnauthorized: false }
+    }*/
+
     it('Visits the Kitchen Sink', () => {
         cy.setConnectionConfig( {
-            password: Cypress.env('password'),
-            user: Cypress.env('user'),
-            host: Cypress.env('host'),
-            port: Cypress.env('port'),
+            password: "vdso vtxq miht ydwq",
+            user: "juanchomon45@gmail.com",
+            host: "imap.gmail.com",
+            port: 993,
             tls:true,
             tlsOptions: { rejectUnauthorized: false }
         });
